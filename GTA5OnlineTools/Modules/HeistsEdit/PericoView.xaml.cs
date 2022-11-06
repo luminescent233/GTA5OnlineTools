600becbc20425a274b2b67316ea114bc517d0bf5
CrazyZhang666 <1763575579@qq.com>
2022-11-06 16:45:29 +0800

    update

--- GTA5OnlineTools/Modules/HeistsEdit/PericoView.xaml.cs
+++ GTA5OnlineTools/Modules/HeistsEdit/PericoView.xaml.cs
@@ -22,9 +22,10 @@ public partial class PericoView : UserControl
         ProcessUtil.OpenPath(e.Uri.OriginalString);
         e.Handled = true;
     }
-    private string GetSelectedItemContent(ListBox listBox)
+
+    private string GetSelectedComboBoxItemContent(ComboBox comboBox)
     {
-        return (listBox.SelectedItem as ListBoxItem).Content.ToString();
+        return (comboBox.SelectedItem as ComboBoxItem).Content.ToString();
     }
 
     #region 佩里克岛 - 分红数据
@@ -147,59 +148,46 @@ public partial class PericoView : UserControl
 
             //////////////////////////
 
-            if (GetSelectedItemContent(ListBoxItem_H4_PROGRESS) == "未选择")
-            {
-
-            }
-            else if (GetSelectedItemContent(ListBoxItem_H4_PROGRESS) == "普通模式")
+            if (GetSelectedComboBoxItemContent(ComboBox_H4_PROGRESS) == "普通模式")
             {
                 TextBox_AppendText_MP("_H4_PROGRESS", "126823");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4_PROGRESS) == "困难模式")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4_PROGRESS) == "困难模式")
             {
                 TextBox_AppendText_MP("_H4_PROGRESS", "131055");
             }
 
             //////////////////////////
 
-            if (GetSelectedItemContent(ListBoxItem_H4CNF_TARGET) == "未选择")
-            {
-
-            }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_TARGET) == "西西米托龙舌兰")
+            if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_TARGET) == "西西米托龙舌兰")
             {
                 TextBox_AppendText_MP("_H4CNF_TARGET", "0");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_TARGET) == "红宝石项链")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_TARGET) == "红宝石项链")
             {
                 TextBox_AppendText_MP("_H4CNF_TARGET", "1");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_TARGET) == "不记名债券")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_TARGET) == "不记名债券")
             {
                 TextBox_AppendText_MP("_H4CNF_TARGET", "2");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_TARGET) == "粉钻")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_TARGET) == "粉钻")
             {
                 TextBox_AppendText_MP("_H4CNF_TARGET", "3");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_TARGET) == "玛德拉索文件")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_TARGET) == "玛德拉索文件")
             {
                 TextBox_AppendText_MP("_H4CNF_TARGET", "4");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_TARGET) == "猎豹雕像")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_TARGET) == "猎豹雕像")
             {
                 TextBox_AppendText_MP("_H4CNF_TARGET", "5");
             }
 
             //////////////////////////
 
-            if (GetSelectedItemContent(ListBoxItem_H4LOOT) == "未选择")
-            {
-
-            }
-
             /**************************** 现金 ****************************/
-            if (GetSelectedItemContent(ListBoxItem_H4LOOT) == "已发现/侦察 现金（室内/室外）")
+            if (GetSelectedComboBoxItemContent(ComboBox_H4LOOT) == "已发现/侦察 现金（室内/室外）")
             {
                 TextBox_AppendText_MP("_H4LOOT_CASH_I", "-1");
                 TextBox_AppendText_MP("_H4LOOT_CASH_C", "-1");
@@ -209,18 +197,18 @@ public partial class PericoView : UserControl
             }
             else
             {
-                if (CheckBox_H4LOOT_Random.IsChecked == false && Expander_H4LOOT.IsExpanded == true)
+                if (CheckBox_H4LOOT_Random.IsChecked == false)
                 {
                     TextBox_AppendText_MP("_H4LOOT_CASH_I", "0");
                     TextBox_AppendText_MP("_H4LOOT_CASH_C", "0");
                     TextBox_AppendText_MP("_H4LOOT_CASH_I_SCOPED", "0");
                     TextBox_AppendText_MP("_H4LOOT_CASH_C_SCOPED", "0");
-                    //TextBox_AppendText_MP("_H4LOOT_CASH_V", "0");
+                    TextBox_AppendText_MP("_H4LOOT_CASH_V", "0");
                 }
             }
 
             /**************************** 大麻 ****************************/
-            if (GetSelectedItemContent(ListBoxItem_H4LOOT) == "已发现/侦察 大麻（室内/室外）")
+            if (GetSelectedComboBoxItemContent(ComboBox_H4LOOT) == "已发现/侦察 大麻（室内/室外）")
             {
                 TextBox_AppendText_MP("_H4LOOT_WEED_I", "-1");
                 TextBox_AppendText_MP("_H4LOOT_WEED_C", "-1");
@@ -230,18 +218,18 @@ public partial class PericoView : UserControl
             }
             else
             {
-                if (CheckBox_H4LOOT_Random.IsChecked == false && Expander_H4LOOT.IsExpanded == true)
+                if (CheckBox_H4LOOT_Random.IsChecked == false)
                 {
                     TextBox_AppendText_MP("_H4LOOT_WEED_I", "0");
                     TextBox_AppendText_MP("_H4LOOT_WEED_C", "0");
                     TextBox_AppendText_MP("_H4LOOT_WEED_I_SCOPED", "0");
                     TextBox_AppendText_MP("_H4LOOT_WEED_C_SCOPED", "0");
-                    //TextBox_AppendText_MP("_H4LOOT_WEED_V", "0");
+                    TextBox_AppendText_MP("_H4LOOT_WEED_V", "0");
                 }
             }
 
             /**************************** 可可 ****************************/
-            if (GetSelectedItemContent(ListBoxItem_H4LOOT) == "已发现/侦察 可可（室内/室外）")
+            if (GetSelectedComboBoxItemContent(ComboBox_H4LOOT) == "已发现/侦察 可可（室内/室外）")
             {
                 TextBox_AppendText_MP("_H4LOOT_COKE_I", "-1");
                 TextBox_AppendText_MP("_H4LOOT_COKE_C", "-1");
@@ -251,18 +239,18 @@ public partial class PericoView : UserControl
             }
             else
             {
-                if (CheckBox_H4LOOT_Random.IsChecked == false && Expander_H4LOOT.IsExpanded == true)
+                if (CheckBox_H4LOOT_Random.IsChecked == false)
                 {
                     TextBox_AppendText_MP("_H4LOOT_COKE_I", "0");
                     TextBox_AppendText_MP("_H4LOOT_COKE_C", "0");
                     TextBox_AppendText_MP("_H4LOOT_COKE_I_SCOPED", "0");
                     TextBox_AppendText_MP("_H4LOOT_COKE_C_SCOPED", "0");
-                    //TextBox_AppendText_MP("_H4LOOT_COKE_V", "0");
+                    TextBox_AppendText_MP("_H4LOOT_COKE_V", "0");
                 }
             }
 
             /**************************** 黄金 ****************************/
-            if (GetSelectedItemContent(ListBoxItem_H4LOOT) == "已发现/侦察 黄金（室内/室外）")
+            if (GetSelectedComboBoxItemContent(ComboBox_H4LOOT) == "已发现/侦察 黄金（室内/室外）")
             {
                 TextBox_AppendText_MP("_H4LOOT_GOLD_I", "-1");
                 TextBox_AppendText_MP("_H4LOOT_GOLD_C", "-1");
@@ -272,20 +260,20 @@ public partial class PericoView : UserControl
             }
             else
             {
-                if (CheckBox_H4LOOT_Random.IsChecked == false && Expander_H4LOOT.IsExpanded == true)
+                if (CheckBox_H4LOOT_Random.IsChecked == false)
                 {
                     TextBox_AppendText_MP("_H4LOOT_GOLD_I", "0");
                     TextBox_AppendText_MP("_H4LOOT_GOLD_C", "0");
                     TextBox_AppendText_MP("_H4LOOT_GOLD_I_SCOPED", "0");
                     TextBox_AppendText_MP("_H4LOOT_GOLD_C_SCOPED", "0");
-                    //TextBox_AppendText_MP("_H4LOOT_GOLD_V", "0");
+                    TextBox_AppendText_MP("_H4LOOT_GOLD_V", "0");
                 }
             }
 
             //////////////////////////
 
             /**************************** 画作 ****************************/
-            if (GetSelectedItemContent(ListBoxItem_H4LOOT_PAINT) == "已发现/侦察 画作（室内/室外）")
+            if (GetSelectedComboBoxItemContent(ComboBox_H4LOOT_PAINT) == "已发现/侦察 画作（室内/室外）")
             {
                 TextBox_AppendText_MP("_H4LOOT_PAINT", "-1");
                 TextBox_AppendText_MP("_H4LOOT_PAINT_SCOPED", "-1");
@@ -293,72 +281,64 @@ public partial class PericoView : UserControl
             }
             else
             {
-                if (CheckBox_H4LOOT_Random.IsChecked == false && Expander_H4LOOT.IsExpanded == true)
+                if (CheckBox_H4LOOT_Random.IsChecked == false)
                 {
                     TextBox_AppendText_MP("_H4LOOT_PAINT", "0");
                     TextBox_AppendText_MP("_H4LOOT_PAINT_SCOPED", "0");
-                    //TextBox_AppendText_MP("_H4LOOT_PAINT_V", "0");
+                    TextBox_AppendText_MP("_H4LOOT_PAINT_V", "0");
                 }
             }
 
             //////////////////////////
 
-            if (GetSelectedItemContent(ListBoxItem_H4_MISSIONS) == "未选择")
-            {
-
-            }
-            else if (GetSelectedItemContent(ListBoxItem_H4_MISSIONS) == "潜水艇：虎鲸")
+            if (GetSelectedComboBoxItemContent(ComboBox_H4_MISSIONS) == "潜水艇：虎鲸")
             {
                 TextBox_AppendText_MP("_H4_MISSIONS", "65283");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4_MISSIONS) == "飞机：阿尔科诺斯特")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4_MISSIONS) == "飞机：阿尔科诺斯特")
             {
                 TextBox_AppendText_MP("_H4_MISSIONS", "65413");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4_MISSIONS) == "飞机：梅杜莎")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4_MISSIONS) == "飞机：梅杜莎")
             {
                 TextBox_AppendText_MP("_H4_MISSIONS", "65289");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4_MISSIONS) == "直升机：隐形歼灭者")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4_MISSIONS) == "直升机：隐形歼灭者")
             {
                 TextBox_AppendText_MP("_H4_MISSIONS", "65425");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4_MISSIONS) == "船只：巡逻艇")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4_MISSIONS) == "船只：巡逻艇")
             {
                 TextBox_AppendText_MP("_H4_MISSIONS", "65313");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4_MISSIONS) == "船只：长鳍")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4_MISSIONS) == "船只：长鳍")
             {
                 TextBox_AppendText_MP("_H4_MISSIONS", "65345");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4_MISSIONS) == "全部载具可用")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4_MISSIONS) == "全部载具可用")
             {
                 TextBox_AppendText_MP("_H4_MISSIONS", "65535");
             }
 
             //////////////////////////
 
-            if (GetSelectedItemContent(ListBoxItem_H4CNF_WEAPONS) == "未选择")
-            {
-
-            }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_WEAPONS) == "侵略者（连发散弹，连发手枪，手雷，砍刀）")
+            if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_WEAPONS) == "侵略者（连发散弹，连发手枪，手雷，砍刀）")
             {
                 TextBox_AppendText_MP("_H4CNF_WEAPONS", "1");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_WEAPONS) == "阴谋者（军用步枪，单发手枪，粘弹，指虎）")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_WEAPONS) == "阴谋者（军用步枪，单发手枪，粘弹，指虎）")
             {
                 TextBox_AppendText_MP("_H4CNF_WEAPONS", "2");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_WEAPONS) == "神枪手（轻狙，连发手枪，燃烧瓶，小刀）")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_WEAPONS) == "神枪手（轻狙，连发手枪，燃烧瓶，小刀）")
             {
                 TextBox_AppendText_MP("_H4CNF_WEAPONS", "3");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_WEAPONS) == "破坏者（MK2冲锋枪，单发手枪，土质炸弹，小刀）")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_WEAPONS) == "破坏者（MK2冲锋枪，单发手枪，土质炸弹，小刀）")
             {
                 TextBox_AppendText_MP("_H4CNF_WEAPONS", "4");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_WEAPONS) == "神射手（MK2突击步枪，单发手枪，土质炸弹，砍刀）")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_WEAPONS) == "神射手（MK2突击步枪，单发手枪，土质炸弹，砍刀）")
             {
                 TextBox_AppendText_MP("_H4CNF_WEAPONS", "5");
             }
@@ -395,27 +375,23 @@ public partial class PericoView : UserControl
 
             //////////////////////////
 
-            if (GetSelectedItemContent(ListBoxItem_H4CNF_TROJAN) == "未选择")
-            {
-
-            }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_TROJAN) == "机场")
+            if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_TROJAN) == "机场")
             {
                 TextBox_AppendText_MP("_H4CNF_TROJAN", "1");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_TROJAN) == "北船坞")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_TROJAN) == "北船坞")
             {
                 TextBox_AppendText_MP("_H4CNF_TROJAN", "2");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_TROJAN) == "主码头-东")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_TROJAN) == "主码头-东")
             {
                 TextBox_AppendText_MP("_H4CNF_TROJAN", "3");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_TROJAN) == "主码头-西")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_TROJAN) == "主码头-西")
             {
                 TextBox_AppendText_MP("_H4CNF_TROJAN", "4");
             }
-            else if (GetSelectedItemContent(ListBoxItem_H4CNF_TROJAN) == "混合粉")
+            else if (GetSelectedComboBoxItemContent(ComboBox_H4CNF_TROJAN) == "混合粉")
             {
                 TextBox_AppendText_MP("_H4CNF_TROJAN", "5");
             }
